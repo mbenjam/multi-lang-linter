1a4b7f27b7a73840fb6f64756a4cb5fee0282f51
mbenjam <mbenjam9@dtcc.edu>
2025-09-20 03:55:43 -0400

    Update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,6 +1,12 @@
- #include <iostream>
-using namespace std;
-int  main( ) {
-int x= 5; int y=10;
-cout<<"Sum:"<<x+ y <<endl;
-return 0; }
+// Copyright 2025 Michael Benjamin
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+  int x = 42;
+  cout << "Hello World: " << x << endl;
+  return 0;
+}
